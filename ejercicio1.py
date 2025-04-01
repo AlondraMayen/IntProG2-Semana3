3e7dccd8d2b7e527450d2d86d1a7f480f936adb5
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-01 13:58:33 -0600

    ejercicios de tarea semana 3

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,9 +2,7 @@ temp_fah = float (input("Ingresa la temperatura en F: "))
 temp_cel = temp_fah - 32
 temp_cel = temp_cel* 5
 temp_cel = temp_cel / 9
-
 temp_cel = ((temp_fah -32 ) * 5)/ 9
-
 temp_kel = temp_cel + 273.15
 print (f"Grados Celsius {temp_cel:.2f}")
 print(f"Grados Kelvin, {temp_kel:.2f}")
---
+++ ejercicio10.py
@@ -0,0 +1,11 @@
+import math
+radio = float(input("Ingrese el radio del cilindro: "))
+altura = float(input("Ingrese la altura del cilindro: "))
+area_base = math.pi * radio ** 2
+volumen = area_base * altura
+area_lateral = 2 * math.pi * radio * altura
+area_superficial = area_lateral + 2 * area_base
+print(f"Radio ingresado: {radio}")
+print(f"Altura ingresada: {altura}")
+print(f"Volumen del cilindro: {volumen:.2f}")
+print(f"Área superficial del cilindro: {area_superficial:.2f}")
