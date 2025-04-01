8f7861a4d0e3f2a1b1cb33a6247a0a2a2c9625d2
AlondraMayen <asmayen@uamv.edu.ni>
2025-04-01 13:35:26 -0600

    Diagramas de flujo

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+base = int(input("Introduzca la base del rectangulo: "))
+altura = int(input("Introduzca la altura del rectangulo: " ))
+area = base * altura
+a = base * 2
+b = altura * 2
+perimetro = a + b
+print 
