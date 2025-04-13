06a8c12d7073a4642d233f85cc8daefdf9375154
Alondra <asmayen@uamv.edu.ni>
2025-04-13 04:37:17 -0600

    Delete ejercicio5.py

--- ejercicio5.py
+++
@@ -1,6 +0,0 @@
-segundos_totales = int(input("Ingrese la cantidad total de segundos: "))
-horas = segundos_totales // 3600
-resto = segundos_totales - (horas * 3600)
-minutos = resto // 60
-segundos = resto - (minutos * 60)
-print(f"{horas} horas, {minutos} minutos, {segundos} segundos")
